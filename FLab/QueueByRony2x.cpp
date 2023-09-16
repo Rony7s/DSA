e182664454c644204b6c607dce1904cb473d6a9a
Rony Ahmmed <30334499+Rony7s@users.noreply.github.com>
2023-09-17 00:13:53 +0600

    Add files via upload

---
+++ FLab/QueueByRony2x.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+using namespace std;
+
+#define Qsize 4
+
+typedef struct {
+    int data [Qsize];
+    int head, tail;
+} Queue;
+
+void enqueue(Queue *q, int item){
+    if((q->tail+1)%(Qsize+1) == q->head){
+        cout <<"Queue is full"<<endl;
+        return;
+    }
+
+    q->data[q->tail] = item;
+    q->tail = (q->tail+1)%(Qsize+1);
+}
+
+int dequeue(Queue *q){
+    if(q->tail == q->head){
+        cout <<"Queue is empty"<<endl;
+        return -1;
+    }
+    int item = q->data[q->head];
+    q->head = (q->head +1)% (Qsize+1);
+    return item;
+}
+
+int main(){
+
+    cout <<"EdTech 4"<<endl;
+    Queue myQ;
+    int item;
+    myQ.head = 0;
+    myQ.tail = 0;
+
+    enqueue (&myQ, 10);
+    cout <<"tail = "<< myQ.tail <<endl;
+
+    enqueue (&myQ, 3);
+    cout <<"tail = "<< myQ.tail <<endl;
+
+    enqueue (&myQ, 5);
+    cout <<"tail = "<< myQ.tail <<endl;
+
+    cout <<"starting head = "<<myQ.head<<endl;
+
+    item  = dequeue (&myQ);
+    cout <<"item = "<< item <<" head = "<<myQ.head<<endl;
+
+    item  = dequeue (&myQ);
+    cout <<"item = "<< item <<" head = "<<myQ.head<<endl;
+
+
+    enqueue (&myQ, 7);
+    cout <<"tail = "<< myQ.tail <<endl;
+
+    enqueue (&myQ, 9);
+    cout <<"tail = "<< myQ.tail <<endl;
+
+    return 0;
+}
